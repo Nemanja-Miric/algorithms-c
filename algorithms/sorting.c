fb9e22ee7596548636f90c83833a3936184b3aa6
Nemanja Mirić <nemanjamiric@protonmail.com>
2025-11-17 17:48:12 +0100

    Implemented testing for Counting sort

--- algorithms/sorting.c
+++ algorithms/sorting.c
@@ -1,18 +1,20 @@
+#include <stddef.h>
+
 #include "sorting.h"
 
 void counting_sort(int *ptr, size_t count, const int k)
 {
-    int count[k + 1] = {};
+    int frequency[k + 1] = {};
     int output[count];
     
     for (size_t i = 0; i < count; i++)
-        count[*(ptr + i)]++;
+        frequency[*(ptr + i)]++;
     
     for (size_t i = 1; i < k + 1; i++)
-        count[i] += count[i - 1];
+        frequency[i] += frequency[i - 1];
     
     for (size_t i = count - 1; i != -1; i--)
-        output[--count[*(ptr + i)]] = *(ptr + i);
+        output[--frequency[*(ptr + i)]] = *(ptr + i);
     
     for (size_t i = 0; i < count; i++)
         *(ptr + i) = output[i];
---
+++ algorithms/sorting.h
@@ -0,0 +1,7 @@
+#ifndef ALGORITHMS_C_SORTING_H
+#define ALGORITHMS_C_SORTING_H
+
+//                                                          Space        | Memory
+void counting_sort(int *ptr, size_t count, const int k); // O(count + k) | O(count + k)
+
+#endif
