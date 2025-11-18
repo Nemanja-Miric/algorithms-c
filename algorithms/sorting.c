4dc17281b1711d40758019ec6e1a081e5d54e02a
Nemanja Mirić <nemanjamiric@protonmail.com>
2025-11-18 16:53:19 +0100

    Optimized Counting sort (no longer a stable sort - instead in-place)

--- algorithms/sorting.c
+++ algorithms/sorting.c
@@ -2,20 +2,15 @@
 
 #include "sorting.h"
 
-void counting_sort(int *ptr, size_t count, const int k)
+void counting_sort(unsigned long long int * restrict ptr, const size_t count, const unsigned long long int k)
 {
-    int frequency[k + 1] = {};
-    int output[count];
+    unsigned long long int frequency[k + 1] = {};
     
     for (size_t i = 0; i < count; i++)
-        frequency[*(ptr + i)]++;
+        frequency[ptr[i]]++;
     
-    for (size_t i = 1; i < k + 1; i++)
-        frequency[i] += frequency[i - 1];
-    
-    for (size_t i = count - 1; i != -1; i--)
-        output[--frequency[*(ptr + i)]] = *(ptr + i);
-    
-    for (size_t i = 0; i < count; i++)
-        *(ptr + i) = output[i];
+    size_t j = 0;
+    for (size_t i = 0; i < k + 1; i++)
+        for ( ; frequency[i]--; j++)
+            ptr[j] = i;
 }
--- algorithms/sorting.h
+++ algorithms/sorting.h
@@ -1,7 +1,7 @@
 #ifndef ALGORITHMS_C_SORTING_H
 #define ALGORITHMS_C_SORTING_H
 
-//                                                          Space        | Memory
-void counting_sort(int *ptr, size_t count, const int k); // O(count + k) | O(count + k)
+//                                                                                                                Space        | Memory
+void counting_sort(unsigned long long int * restrict ptr, const size_t count, const unsigned long long int k); // O(count + k) | O(count + k)
 
 #endif
